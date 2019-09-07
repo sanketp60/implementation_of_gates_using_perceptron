4995f408c59b96bdd27747310e1c786ff569d409
sanketp60 <spisat@ghotmail.com>
2019-09-07 21:29:20 +0530

    added comments and code cleanup

--- or_gate.py
+++ or_gate.py
@@ -1,9 +1,11 @@
-alpha=1
-w1,w2,b=0,0,0
-t=[-1,1,1,1]
-x1=[-1,-1,1,1]
-x2=[-1,1,-1,1]
+#Initialize parameters
+alpha=1 #Training speed
+w1,w2,b=0,0,0 #weights and biases initialized
+t=[-1,1,1,1] #declare target
+x1=[-1,-1,1,1] #declare input 1
+x2=[-1,1,-1,1] #declare input 2
 
+#method to convert calculated o/p to actual o/p
 def calc_yact(num):
     if(num>0):
         return 1
@@ -12,19 +14,21 @@ def calc_yact(num):
     else:
         return -1
 
+#calculation function
 def calc(x1,x2,t):  
     global w1
     global w2
     global b
     global alpha
     yact=calc_yact(w1*x1+w2*x2+b)
-    
-    while(yact!=t):
+
+    #turing weights and biases if target not matched
+    if(yact!=t):
         w1=w1+alpha*t*x1
         w2=w2+alpha*t*x2
         b=b+alpha*t
-        yact=calc_yact(w1*x1+w2*x2+b)
-    
+
+#executing loop for input sequence
 for i in range(0,4):
     calc(x1[i],x2[i],t[i])
 print(f'w1={w1}, w2={w2}, b={b}')
